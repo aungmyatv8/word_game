0e60f6de46bd864286903ebaf602e3cb6d0c07c4
Aung Myint Myat <aungmyatv8@gmail.com>
2023-05-16 16:09:04 +0900

    socket io and mongodb setup

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://aungmyintmyat:veomas123@cluster0.6q07t2s.mongodb.net/?retryWrites=true&w=majority")
-.then(result => console.log("result", result))
+mongoose.connect("mongodb+srv://aungmyintmyat:veomas123@cluster0.18eromm.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true },)
+.then(_ => console.log("mongodb connected"))
 .catch(err => console.error(err))
 
 app.get("/test", (_, res) => res.json({message: "hello"}))
