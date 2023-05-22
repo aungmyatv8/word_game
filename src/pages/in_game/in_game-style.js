d5b8c10c03dc49f06f74e4e157b41012e12e0630
Aung Myint Myat <aungmyatv8@gmail.com>
2023-05-22 16:46:36 +0900

    chat ui

---
+++ src/pages/in_game/in_game-style.js
@@ -0,0 +1,69 @@
+import { createStyles } from "@mantine/core";
+
+export const useStyles = createStyles(() => ({
+   background: {
+    width: "100vw",
+    margin: "5px",
+    minHeight: "90vh",
+    // display: "flex",
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+    borderRadius: "10px",
+   },
+
+   chatContainer: {
+    display: "flex",
+    flexDirection: "column",
+    height: "90vh",
+    // background: "green",
+    position: "relative"
+   },
+   chat: {
+    height: "82vh",
+    // height: "100vh",
+    overscrollBehavior: "auto",
+    overflow: "auto",
+    borderBottom: "1px solid rgba(255, 255, 255, 0.3)",
+    // backgroundColor: "green",
+   },
+   input: {
+    flexGrow: "2",
+    // alignItems: "flex-end"
+    // alignSelf: "flex-e"
+    position: "absolute",
+    width: "100%",
+    bottom: "0px",
+    marginBottom: "10px",
+   },
+   leftChat: {
+    display: "flex",
+    // color: "black",
+    margin: "10px",
+    // backgroundColor: "#70d6ff"
+   },
+   rightChat: {
+    display: "flex",
+    margin: "0 10px",
+    flexDirection: "row-reverse", 
+   },
+   leftWord: {
+    backgroundColor: "#6d8afd",
+    position: "relative",
+    top: "-17px",
+    marginLeft: "10px",
+    borderRadius: "10px",
+    padding: "10px",
+    color:"black"
+   },
+
+   rightWord: {
+    backgroundColor: "#9381ff",
+    position: "relative",
+    top: "-17px",
+    marginRight: "10px",
+    borderRadius: "10px",
+    padding: "10px",
+    color:"black"
+   },
+ 
+  }));
+  
--- src/pages/in_game/in_game.js
+++ src/pages/in_game/in_game.js
@@ -1,22 +1,65 @@
-
-import React, { useState, useCallback, useEffect} from "react";
-import { AppShell, Center, Title, Button, Container, Loader } from "@mantine/core";
+import React, { useState, useCallback, useEffect } from "react";
+import { Avatar } from '@mantine/core';
+import {IconAt} from '@tabler/icons-react'
+import {
+  AppShell,
+  Center,
+  Title,
+  Button,
+  Container,
+  Loader,
+  Input,
+} from "@mantine/core";
 import Aside from "../../component/Navbar/navbar";
 import Protected from "../../component/Protected";
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
+import { useStyles } from "./in_game-style";
 
 const InGame = () => {
-    return <AppShell navbar={<Aside active={1} />}>
-    <Container>
-      <Center>
-        <Title>Find Match</Title>
-      </Center>
-      <Center>
-        
-      </Center>
-    </Container>
-  </AppShell>
-}
 
+
+  const Chat = () => {
+    return <div className={classes.chat}>
+         {
+          [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20].map(() => (<React.Fragment>
+            <div className={classes.leftChat}>
+               <Avatar color="cyan">MK</Avatar>
+                <p className={classes.leftWord}>Left Message</p>
+              </div>
+
+              <div className={classes.rightChat}>
+                <Avatar color="cyan">MK</Avatar>
+                <p className={classes.rightWord}>Right Message</p>
+              </div>
+          </React.Fragment>))
+         }     
+            
+    </div>
+  }
+
+  
+
+  const { classes } = useStyles();
+  return (
+    <AppShell navbar={<Aside active={1} />}>
+      <Container>
+        <Center>
+          <Title>Game</Title>
+        </Center>
+        <Center>
+          <Container className={classes.background}>
+            <div className={classes.chatContainer}>
+             {Chat()}
+             
+
+            <Input icon={"G"} placeholder="Word" className={classes.input} />
+            </div>
+         
+          </Container>
+        </Center>
+      </Container>
+    </AppShell>
+  );
+};
 
 export default InGame;
